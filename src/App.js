2591ac4586e10eba3e5d49fab22442e7750d3666
Nhan Phan <nphanacc@gmail.com>
2021-09-10 17:24:13 -0700

    Able to show display content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import React from "react";
 
-function App() {
-  return (
-    <div >
-     Hi
-    </div>
-  );
+import Crossword from "@jaredreisinger/react-crossword";
+
+const data = {
+	across: {
+    1: {
+      clue: 'one plus one',
+      answer: 'TWO',
+      row: 0,
+      col: 0,
+    },
+  },
+  down: {
+    2: {
+      clue: 'three minus two',
+      answer: 'ONE',
+      row: 0,
+      col: 2,
+    },
+  },
+};
+
+const App = () => {
+	return<div> <Crossword data={data} /> </div>;
 }
 
 export default App;
